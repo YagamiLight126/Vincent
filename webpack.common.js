f4c30c2b962a6028f159456a485149cccc249ee8
fuzhicheng <fuzhicheng94@gmail.com>
2022-08-01 18:40:19 +0800

    update deps

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -36,7 +35,6 @@ module.exports = {
   },
 
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({ template: path.resolve("./src/index.html") }),
   ],
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,4 @@
-const webpack = require("webpack");
+const path = require("path");
 const { merge } = require("webpack-merge");
 const AntdDayjsWebpackPlugin = require("antd-dayjs-webpack-plugin");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
@@ -8,15 +8,12 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production", // 会将 DefinePlugin 中 process.env.NODE_ENV 的值设置为 production。启用 FlagDependencyUsagePlugin, FlagIncludedChunksPlugin, ModuleConcatenationPlugin, NoEmitOnErrorsPlugin, OccurrenceOrderPlugin, SideEffectsFlagPlugin 和 TerserPlugin。
-  target: "browserslist",
-  output: { filename: "[name].[chunkhash:8].js" },
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[chunkhash:8].js",
+    clean: true,
+  },
   devtool: false,
-
-  plugins: [
-    new AntdDayjsWebpackPlugin(),
-    new BundleAnalyzerPlugin(),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": "production",
-    }),
-  ],
+  plugins: [new AntdDayjsWebpackPlugin(), new BundleAnalyzerPlugin()],
 });
