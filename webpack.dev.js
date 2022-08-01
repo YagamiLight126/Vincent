ea307b476c0086164a8320f3071d83ea06d4223b
fuzhicheng <fuzhicheng94@gmail.com>
2022-08-01 21:54:24 +0800

    update-deps

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const { merge } = require("webpack-merge");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
@@ -15,7 +16,7 @@ module.exports = merge(common, {
         test: /\.[jt]sx?$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: require.resolve("babel-loader"),
           options: {
             plugins: [require.resolve("react-refresh/babel")].filter(Boolean),
           },
@@ -27,17 +28,14 @@ module.exports = merge(common, {
   plugins: [new ReactRefreshWebpackPlugin()].filter(Boolean),
 
   devServer: {
-    contentBase: "./dist",
-    host: "0.0.0.0",
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
     port: 1231, // 指定端口号以侦听以下请求：
     hot: true, // 启用 webpack 的 Hot Module Replacement 功能：
-    clientLogLevel: "silent",
+    client: { logging: "info" },
     historyApiFallback: true, // 当使用 HTML5 History API 时, 所有的 404 请求都会响应 index.html 的内容
-    compress: true, // 为每个静态文件开启 gzip compression：
-    stats: {
-      modules: false, // 告知 stats 是否添加关于构建模块的信息
-    },
-    useLocalIp: true, // 此选项使浏览器可以使用的本地IP打开。
+    compress: true, // 为每个静态文件开启 gzip compression
     // proxy: {
     //   "/api": {
     //     target: "", // 填写某个地址
