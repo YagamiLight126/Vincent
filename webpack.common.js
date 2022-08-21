e8a9be483784e67eb722103ab47a142d5a6dc498
傅智诚 <shirley@fuzhichengdeMacBook-Air.local>
2022-08-21 22:17:48 +0800

    chore: update webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -31,7 +32,7 @@ module.exports = {
       {
         test: /\.less$/,
         use: [
-          "style-loader",
+          MiniCssExtractPlugin.loader,
           "css-loader",
           {
             loader: "less-loader",
@@ -46,6 +47,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "./src/index.html"),
     }),
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash:8].css" }),
   ],
 
   optimization: {
